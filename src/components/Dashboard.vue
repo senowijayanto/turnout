9d6b3119654f77df271bafb8d78b4b82bacf4018
Seno Wijayanto <senowijayanto@gmail.com>
2018-08-17 00:05:32 +0700

    Signing Out

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,22 @@
 <template lang="html">
   <div class="">
-    Dashboard Component
-
+    <h3>Events Dashboard</h3>
+    <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
     {{$store.state}}
   </div>
 </template>
 
 <script>
+import { firebaseApp } from '../firebaseApp'
+require('firebase/auth')
+
 export default {
+  methods: {
+    signOut() {
+      this.$store.dispatch('signOut')
+      firebaseApp.auth().signOut()
+    }
+  }
 }
 </script>
 
