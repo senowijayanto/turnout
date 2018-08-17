9136f5342a59bdc460419d3942fd0cc26f80b4de
Seno Wijayanto <senowijayanto@gmail.com>
2018-08-17 21:58:06 +0700

    Developing the Sign Up Component

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,14 +1,15 @@
 <template lang="html">
   <div class="form-inline">
-    <h3>Sign In</h3>
+    <h3>Sign Up</h3>
     <div class="form-group">
       <input type="text" placeholder="Your email" class="form-control" v-model="email" autocomplete="false">
       <input type="password" placeholder="Your password" class="form-control" v-model="password">
-      <button class="btn btn-primary" @click="signIn">Sign In</button>
+      <button class="btn btn-primary" @click="signUp">Sign Up</button>
+      <br>
     </div>
     <br>
     <br>
-    <router-link to="/signup">Not a user? Sign Up</router-link>
+    <router-link to="/signin">Already a user? Sign In</router-link>
     <br>
     <p>{{error.message}}</p>
   </div>
@@ -29,8 +30,8 @@ export default {
     }
   },
   methods: {
-    signIn() {
-      firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+    signUp() {
+      firebaseApp.auth().createUserWithEmailAndPassword(this.email, this.password)
         .catch(error => {
           this.error = error
         })
